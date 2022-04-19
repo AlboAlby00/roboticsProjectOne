6493fa978e4151867f7d88bbf9e5ed478d2c8687
alboalby00 <10676663@polimi.it>
2022-04-19 19:38:21 +0200

    changed project name from roboticsProjectOne to robotics_project_one, added robot size from param server

--- src/odom.cpp
+++ src/odom.cpp
@@ -8,14 +8,23 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "odometryNode");
   ros::NodeHandle n;
 
+  double r,lx,ly;
+  n.getParam("/r", r);
+  n.getParam("/lx", lx);
+  n.getParam("/ly", ly);
+
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::TwistStamped>("cmd_vel", 1000);
 
-  double w1=5.0,w2=7.0,w3=4.0,w4=2.0;
+  double w1=5.0,w2=7.0,w3=4.0,w4=3.0;
   double vx=w2 ,vy = w4+w3;
   double w = w1;
   ros::Rate loop_rate(10);
 
   while (ros::ok()) {
+
+    vx=(w1+w2+w3+w4)*r/4;
+    vy=(-w1+w2+w3-w4)*r/4;
+    w=(-w1+w2-w3+w4)*r/(4*(lx+ly));
 
     // generate cmd_vel msg
     geometry_msgs::TwistStamped msg;
@@ -31,7 +40,7 @@ int main(int argc, char **argv) {
 
 
     // print count to screen
-    //ROS_INFO("%f", msg.twist.angular.z);
+    ROS_INFO("r: %f,lx: %f,ly: %f",r,lx,ly );
 
     // publish messages
     cmd_vel_pub.publish(msg);
