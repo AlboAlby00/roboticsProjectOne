7cf07e1104c53b583c1745b31af05f0cc65090fa
Matteo Delpini <matteo.delpini@mail.polimi.it>
2022-04-24 21:45:11 +0200

    Add euler and runge-kutta odom, message generation

--- src/compute_control.cpp
+++ src/compute_control.cpp
@@ -9,6 +9,7 @@ class ComputeControl{
         ros::Subscriber sub_cmd_vel;
         ros::Publisher pub_wheels_rpm;
         double r,l_x,l_y,T;   //parameters that will be retrieved from the parameter server
+        int seq;
 
     public:
         ComputeControl(){
@@ -21,8 +22,10 @@ class ComputeControl{
             n.getParam("/lx",l_x);
             n.getParam("/ly",l_y);
             n.getParam("/T",T);
-        }
 
+            //initialize frame_id
+            seq = 1;
+        }
 
         void callback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
             robotics_project_one::WheelSpeeds response;
@@ -31,10 +34,17 @@ class ComputeControl{
             double v_y = msg->twist.linear.y;
             double w = msg->twist.angular.z;
 
+            //calculate speeds
             response.rpm_fl = 60*T*(v_x-v_y-(l_x+l_y)*w)/r;
             response.rpm_fr = 60*T*(v_x+v_y+(l_x+l_y)*w)/r;
             response.rpm_rl = 60*T*(v_x+v_y-(l_x+l_y)*w)/r;
             response.rpm_rr = 60*T*(v_x-v_y+(l_x+l_y)*w)/r;
+
+            //generate header
+            response.header.stamp = ros::Time::now();
+            response.header.frame_id = "base_link";
+            response.header.seq = seq;
+            seq++;
 
             pub_wheels_rpm.publish(response);
         }
--- src/odom.cpp
+++ src/odom.cpp
@@ -4,10 +4,8 @@
 #include <geometry_msgs/TwistStamped.h>
 #include <sensor_msgs/JointState.h>
 #include <nav_msgs/Odometry.h>
+#include <tf2/LinearMath/Quaternion.h>
 #include <cmath>
-
-
-
 
 bool reset_odometry_callback(double *x,double *y,double *theta, robotics_project_one::Reset_odometry::Request  &req, 
                     robotics_project_one::Reset_odometry::Response &res) {
@@ -20,10 +18,7 @@ bool reset_odometry_callback(double *x,double *y,double *theta, robotics_project
   return true;
 }
 
-
-
-//todo: subscribe to wheel_states topic and get ticks for the 4 wheels
-//      use the formulas to compute odometry and publish it
+//TODO: use the formulas to compute odometry and publish it
 //      broadcast TF from world frame to base frame (?)
 class OdometryNode{
   private:
@@ -50,18 +45,18 @@ class OdometryNode{
 
     double v_x,v_y,w;
 
-    
-    
+    int seq;
 
   public:
     OdometryNode(){
 
       pub_cmd_vel= n.advertise<geometry_msgs::TwistStamped>("cmd_vel", 1000);
       pub_odom = n.advertise<nav_msgs::Odometry>("odom",1000);
+
       sub_wheel_states = n.subscribe("wheel_states", 1000, &OdometryNode::callback, this);
 
       srv_reset_odometry =
-      n.advertiseService<robotics_project_one::Reset_odometry::Request, 
+                n.advertiseService<robotics_project_one::Reset_odometry::Request, 
                          robotics_project_one::Reset_odometry::Response>("reset_odometry", 
           boost::bind(&reset_odometry_callback, &x,&y,&theta, _1, _2) 
       );
@@ -72,7 +67,8 @@ class OdometryNode{
       n.getParam("/ly",l_y);
       n.getParam("/T",T);
       n.getParam("/N",N);
-
+      
+      //retrieve initial position
       n.getParam("/x0",x);
       n.getParam("/y0",y);
       n.getParam("/theta0",theta);
@@ -80,10 +76,9 @@ class OdometryNode{
 
       firstMsg=true;
 
+      seq=1;
 
     }
-
-    
 
     void callback(const sensor_msgs::JointState& msg){
       double ticks_fl = msg.position[0];
@@ -95,26 +90,28 @@ class OdometryNode{
 
       ros::Time current_time = msg.header.stamp;
 
-      if(firstMsg){
-        previous_ticks_fl = ticks_fl;
-        previous_ticks_fr = ticks_fr;
-        previous_ticks_rl = ticks_rl;
-        previous_ticks_rr = ticks_rr;
-        previous_time = current_time;
+      if(!firstMsg){
+        ros::Duration dt = current_time-previous_time;
+        w_fl = calculate_wheel_speed(previous_ticks_fl,ticks_fl,dt);
+        w_fr = calculate_wheel_speed(previous_ticks_fr,ticks_fr,dt);
+        w_rl = calculate_wheel_speed(previous_ticks_rl,ticks_rl,dt);
+        w_rr = calculate_wheel_speed(previous_ticks_rr,ticks_rr,dt);
 
-        firstMsg = false;
-        return;
+        generate_cmd_vel(w_fl,w_fr,w_rl,w_rr);
+
+        calculate_odometry(dt);
+
+        broadcast_tf();
       }
 
-      ros::Duration dt=current_time-previous_time;
-      w_fl = calculate_wheel_speed(previous_ticks_fl,ticks_fl,dt);
-      w_fr = calculate_wheel_speed(previous_ticks_fr,ticks_fr,dt);
-      w_rl = calculate_wheel_speed(previous_ticks_rl,ticks_rl,dt);
-      w_rr = calculate_wheel_speed(previous_ticks_rr,ticks_rr,dt);
+      previous_ticks_fl = ticks_fl;
+      previous_ticks_fr = ticks_fr;
+      previous_ticks_rl = ticks_rl;
+      previous_ticks_rr = ticks_rr;
+      previous_time = current_time;
 
-      generate_cmd_vel(w_fl,w_fr,w_rl,w_rr);
-
-      calculate_odometry();
+      firstMsg = false;
+      return;
     }
 
     void generate_cmd_vel(double w_fl,double w_fr,double w_rl,double w_rr){
@@ -123,14 +120,24 @@ class OdometryNode{
       v_y = (-w_fl + w_fr + w_rl - w_rr) * r/4;
       w = (-w_fl + w_fr - w_rl + w_rr) * r/(4*(l_x+l_y));
 
-      // generate cmd_vel msg
+      // generate cmd_vel message
+
       geometry_msgs::TwistStamped msg;
 
+      //generate header
+      msg.header.stamp = ros::Time::now();
+      msg.header.frame_id = "base_link";
+      msg.header.seq = seq;
+
+      //generate twist
+      msg.twist.angular.x = 0;
+      msg.twist.angular.y = 0;
       msg.twist.angular.z = w;
       msg.twist.linear.x = v_x;
       msg.twist.linear.y = v_y;
+      msg.twist.linear.z = 0;
 
-      // publish messages
+      // publish message
       pub_cmd_vel.publish(msg);
     }
 
@@ -138,14 +145,77 @@ class OdometryNode{
       return (curr_ticks-pre_ticks)*2*M_PI/(dt.toSec()*N*T);
     }
 
-    void calculate_odometry(){
-      //todo:choose between euler and runge-kutta
+    void calculate_odometry(ros::Duration dt){
+      //TODO:choose between euler and runge-kutta with dynamic configuration. For now only euler integration is used 
+      
+      //converting from base to world frame
+      double v_wx = v_x*cos(theta) - v_y*sin(theta);
+      double v_wy = v_x*sin(theta) + v_y*cos(theta);
+
+      //create odometry message
+      nav_msgs::Odometry msg = euler_odometry(v_wx,v_wy,dt);
+
+      //publish message
+      pub_odom.publish(msg);
     }
 
-  
+    void generate_message(nav_msgs::Odometry* msg, double v_wx, double v_wy){
+      //generate position
+      msg->pose.pose.position.x = x;
+      msg->pose.pose.position.y = y;
 
+      //generate orientation using tf2::Quaternion::setRPY(x,y,theta)
+      tf2::Quaternion q;
+      q.setRPY(0, 0, theta);
+      msg->pose.pose.orientation.w = q.getW();
+      msg->pose.pose.orientation.x = q.getX();
+      msg->pose.pose.orientation.y = q.getY();
+      msg->pose.pose.orientation.z = q.getZ();
+
+      //generate twist
+      msg->twist.twist.angular.z = w;
+      msg->twist.twist.linear.x = v_wx;
+      msg->twist.twist.linear.y = v_wy;
+
+      //generate header
+      msg->header.stamp = ros::Time::now();
+      msg->header.frame_id = "odom";
+      msg->header.seq = seq;
+      msg->child_frame_id = "base_link";
+      seq++;
+    }
+
+    nav_msgs::Odometry euler_odometry(double v_wx,double v_wy,ros::Duration dt){
+      nav_msgs::Odometry msg;
+
+      //odometry: Euler integration
+      x = x + v_wx*dt.toSec();
+      y = y + v_wy*dt.toSec();
+      theta = theta + w*dt.toSec();
+
+      generate_message(&msg,v_wx,v_wy);
+
+      return msg;
+    }
+
+    nav_msgs::Odometry runge_kutta_odometry(double v_wx,double v_wy,ros::Duration dt){
+      nav_msgs::Odometry msg;
+
+      //odometry: Runge Kutta integration
+      double adjustment = w*dt.toSec()/2.0; 
+      x = x + (v_wx*cos(adjustment) - v_wy*sin(adjustment))*dt.toSec();
+      y = y + (v_wy*cos(adjustment) + v_wx*sin(adjustment))*dt.toSec();
+      theta = theta + w*dt.toSec();
+
+      generate_message(&msg,v_wx,v_wy);
+      
+      return msg; 
+    }
+
+    void broadcast_tf(){//TODO: see if it's better to move trasnform operations on a new node subscribed to the odom topic
+      return;
+    }
 };
-
 
 int main(int argc, char **argv) {
 
